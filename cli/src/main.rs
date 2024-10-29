61ce0d632794b672c46daa4cf022d90596371022
NaruseNia <notshinsin0000@gmail.com>
2024-10-29 15:42:06 +0900

    Enumerate dirs

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about=None)]
+struct Args {
+    #[arg(short, long)]
+    path: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:?}", args.path);
+
+    let dirs: Vec<String> = fs::read_dir(args.path.clone())
+        .unwrap()
+        .filter(|d| d.as_ref().unwrap().path().is_dir() )
+        .map(|d| d.unwrap().path().to_str().unwrap().to_string())
+        .collect();
+
+    println!("{:?}", dirs);
 }
