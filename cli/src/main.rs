10b4acfc8e52b19f88d564a9a426c2a718baa433
NaruseNia <notshinsin0000@gmail.com>
2024-11-02 11:43:16 +0900

    Migrate function to library

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,8 +1,4 @@
 use clap::{Parser, Subcommand};
-use indextree::{Arena, NodeId};
-use serde::Serialize;
-use serde_indextree::Node;
-use std::fs;
 use std::{env, fs::File, io::Write, path::PathBuf};
 
 #[derive(Parser, Debug)]
@@ -21,35 +17,21 @@ enum Commands {
     },
 }
 
-#[derive(Serialize, Debug)]
-struct DirNode {
-    path: String,
-}
-
 fn main() {
     let args = Args::parse();
 
     match args.command {
         Commands::Create { root, output } => {
-            let mut arena: Arena<DirNode> = Arena::new();
-            let root_node = arena.new_node(DirNode {
-                path: String::from("root"),
-            });
-
-            travel(&root, &mut arena, root_node);
-
             if let Some(output) = output {
                 env::set_current_dir(PathBuf::from(&output)).unwrap();
             } else {
                 env::set_current_dir(PathBuf::from(&root)).unwrap();
             }
 
-            let data = &Node::new(root_node, &arena);
-            let byted = bson::to_bson(&data).unwrap();
+            let res = progetto::create_folder_structure(root);
 
             let mut file = File::create("struct.prg").unwrap();
-            file.write_all(bson::to_vec(&byted).unwrap().as_slice())
-                .unwrap();
+            file.write_all(&res).unwrap();
 
             println!(
                 "Create folder structure to: {:?}",
@@ -57,29 +39,4 @@ fn main() {
             );
         }
     }
-}
-
-fn travel(path: &str, arena: &mut Arena<DirNode>, parent: NodeId) {
-    let dirs = get_dirs(path);
-    for entry in dirs {
-        let dir_node = arena.new_node(DirNode {
-            path: entry
-                .file_name()
-                .as_ref()
-                .unwrap()
-                .to_str()
-                .unwrap()
-                .to_string(),
-        });
-        parent.append(dir_node, arena);
-        travel(entry.to_str().unwrap(), arena, dir_node);
-    }
-}
-
-fn get_dirs(path: &str) -> Vec<PathBuf> {
-    fs::read_dir(path)
-        .unwrap()
-        .filter(|entry| entry.as_ref().unwrap().path().is_dir())
-        .map(|entry| entry.unwrap().path())
-        .collect()
 }
--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,14 +1,49 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{fs, path::PathBuf};
+
+use indextree::{Arena, NodeId};
+use serde::Serialize;
+use serde_indextree::Node;
+
+#[derive(Serialize, Debug)]
+pub struct DirNode {
+    path: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn create_folder_structure(root: String) -> Vec<u8> {
+    let mut arena: Arena<DirNode> = Arena::new();
+    let root_node = arena.new_node(DirNode {
+        path: String::from("root"),
+    });
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    travel(&root, &mut arena, root_node);
+
+    let data = &Node::new(root_node, &arena);
+    let out_data = bson::to_bson(&data).unwrap();
+
+    bson::to_vec(&out_data).unwrap()
+}
+
+fn travel(path: &str, arena: &mut Arena<DirNode>, parent: NodeId) {
+    let dirs = get_dirs(path);
+    for entry in dirs {
+        let dir_node = arena.new_node(DirNode {
+            path: entry
+                .file_name()
+                .as_ref()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string(),
+        });
+        parent.append(dir_node, arena);
+        travel(entry.to_str().unwrap(), arena, dir_node);
     }
+}
+
+fn get_dirs(path: &str) -> Vec<PathBuf> {
+    fs::read_dir(path)
+        .unwrap()
+        .filter(|entry| entry.as_ref().unwrap().path().is_dir())
+        .map(|entry| entry.unwrap().path())
+        .collect()
 }
