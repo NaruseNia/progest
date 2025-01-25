1bce9205c5bdd9584458e48ccb5095f14d02e603
NaruseNia <notshinsin0000@gmail.com>
2025-01-25 13:53:34 +0900

    Setup: specta

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,14 +1,29 @@
+use specta_typescript::Typescript;
+use tauri_specta::{collect_commands, Builder};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
+#[specta::specta]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let builder = Builder::<tauri::Wry>::new().commands(collect_commands![greet]);
+
+    #[cfg(debug_assertions)]
+    builder
+        .export(Typescript::default(), "../src/lib/binding.ts")
+        .expect("Failed to export typescript bindings");
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(builder.invoke_handler())
+        .setup(move |app| {
+            builder.mount_events(app);
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
