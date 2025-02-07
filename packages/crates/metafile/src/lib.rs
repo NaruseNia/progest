af2fa3bb3f2f5bedf8fd2b192d087b47dc82f730
NaruseNia <notshinsin0000@gmail.com>
2025-02-07 10:15:46 +0900

    Impl: compress mode

--- packages/crates/metafile/src/lib.rs
+++ packages/crates/metafile/src/lib.rs
@@ -2,13 +2,14 @@ use std::{
     collections::{HashMap, HashSet},
     ffi::OsStr,
     fs,
+    io::Read,
     os::windows::fs::MetadataExt,
     path::Path,
     time::SystemTime,
 };
 
+use brotli::{CompressorReader, Decompressor};
 use getset::{Getters, MutGetters};
-use rmp_serde::{Deserializer, Serializer};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
@@ -40,10 +41,10 @@ pub enum RelationType {
 /// Metafile is a struct that represents a file or folder in the system.
 /// It contains metadata about the file or folder.
 /// The properties field is a generic type that can be used to store additional metadata.
-#[derive(Getters, MutGetters, Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Getters, MutGetters, Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub struct Metafile<P = MetaProperty> {
     #[getset(get = "pub")]
-    guid: String,
+    uid: String,
 
     #[getset(get = "pub")]
     name: String,
@@ -112,6 +113,30 @@ impl Metafile {
     pub fn deserialize(data: Vec<u8>) -> Self {
         rmp_serde::from_slice(&data).unwrap()
     }
+
+    pub fn deserialize_compressed(data: &Vec<u8>) -> Self {
+        let decompressed_data = Self::decompress_data(data);
+        rmp_serde::from_slice(&decompressed_data).unwrap()
+    }
+
+    pub fn serialize_compressed(&self) -> Vec<u8> {
+        let serialized_data = rmp_serde::to_vec_named(&self).unwrap();
+        Self::compress_data(serialized_data)
+    }
+
+    fn compress_data(data: Vec<u8>) -> Vec<u8> {
+        let mut compressor = CompressorReader::new(data.as_slice(), 4096, 11, 22);
+        let mut compressed_bytes = Vec::new();
+        compressor.read_to_end(&mut compressed_bytes).unwrap();
+        compressed_bytes
+    }
+
+    fn decompress_data(data: &Vec<u8>) -> Vec<u8> {
+        let mut decompressor = Decompressor::new(data.as_slice(), 4096);
+        let mut decompressed_bytes = Vec::new();
+        decompressor.read_to_end(&mut decompressed_bytes).unwrap();
+        decompressed_bytes
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
@@ -129,7 +154,7 @@ impl Relation {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub struct MetaProperty {}
 
 pub fn create_meta_from_file(path: &Path) -> Metafile {
@@ -138,7 +163,7 @@ pub fn create_meta_from_file(path: &Path) -> Metafile {
 
 pub fn create_meta_from_file_with_property<P>(path: &Path, property: P) -> Metafile<P> {
     Metafile {
-        guid: uuidv7::create(),
+        uid: ulid::Ulid::new().to_string(),
         raw_path: fs::canonicalize(path)
             .unwrap()
             .into_os_string()
@@ -171,17 +196,21 @@ pub fn create_meta_from_file_with_property<P>(path: &Path, property: P) -> Metaf
 
 #[cfg(test)]
 mod tests {
-    use std::env;
-
     use super::*;
 
     #[test]
     fn test_create_meta_from_file() {
-        println!("{:?}", env::current_dir().unwrap());
         let path = Path::new("./test_files/test.txt");
         let meta = create_meta_from_file(path);
         assert_eq!(meta.name(), "test.txt");
         assert_eq!(meta.ext(), "txt");
         assert_eq!(meta.meta_type(), &MetaType::File);
+
+        let path_out = format!("./test_files/{}@{}.pgmeta", meta.name(), meta.uid());
+        fs::write(&path_out, meta.serialize_compressed()).unwrap();
+        let meta_out = Metafile::deserialize_compressed(&fs::read(&path_out).unwrap());
+
+        assert_eq!(meta, meta_out);
+        let _ = fs::remove_file(path_out);
     }
 }
